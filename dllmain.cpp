76eab771ca8a42d722acfa2f4a06a01e803c6a4d
elpoep <elpoepstudios@gmail.com>
2024-08-23 23:15:15 +0300

    Create dllmain.cpp

---
+++ dllmain.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <csignal>
+#include <thread>
+#include <chrono>
+#include "discord.h"
+
+#define GMEXPORT extern "C" _declspec(dllexport)
+
+struct DiscordState {
+	std::unique_ptr<discord::Core> core;
+};
+
+DiscordState state{};
+
+GMEXPORT double setActivity(char* name, char *state_text, char *large_text)
+{
+	discord::Activity activity{};
+	activity.SetDetails(name);
+	activity.SetState(state_text);
+	activity.GetAssets().SetLargeImage("home");
+	activity.GetAssets().SetLargeText(large_text);
+	activity.SetType(discord::ActivityType::Listening);
+	discord::ActivityTimestamps tmstp{};
+	tmstp.SetStart(time(NULL));
+	activity.GetTimestamps() = tmstp;
+
+	state.core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {});
+
+	return 1;
+}
+
+GMEXPORT double setActivityType(double type) {
+	discord::Activity activity{};
+	activity.SetType((discord::ActivityType)(int)type);
+
+	state.core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {});
+
+	return 1;
+}
+
+GMEXPORT double clearActivity()
+{
+	state.core->ActivityManager().ClearActivity([](discord::Result result) {});
+	return 1;
+}
+
+GMEXPORT double callback()
+{
+	auto result = state.core->RunCallbacks();
+	if (result == discord::Result::Ok) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+GMEXPORT double initialize()
+{
+	discord::Core* core{};
+	discord::Core::Create(1275938097200238725, DiscordCreateFlags_NoRequireDiscord, &core);
+
+	state.core.reset(core);
+
+	if (!state.core) {
+		return 0;
+	}
+
+	return 1;
+}
